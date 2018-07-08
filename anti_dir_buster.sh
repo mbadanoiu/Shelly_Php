c4ca02edff33284b057502d20fc1fe2082a841d2
guest <guest@localhost.localdomain>
2018-07-08 09:45:55 +0000

    More tests

--- anti_dir_buster.sh
+++ anti_dir_buster.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-dirs="./"
+dirs="./yyy.php/"
 
 for i in {1..100}; do
-	dirs="$dirs$i/"
+	dirs="$dirs$i.php/"
 	mkdir -p $dirs
+	cp wrong.php $dirs
 done
 
 cp wrong.php $dirs
 
 for j in {1..100}; do
-	mkdir -p "new_$j"
-	cp -r 1 "new_$j"
+	mkdir -p "./yyy.php/new_$j/"
+	cp -r ./yyy.php/1.php/ "./yyy.php/new_$j.php/"
 done
